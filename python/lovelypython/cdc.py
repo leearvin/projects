7647b84ddc90d50fd9b5da228693a10db1d3e2c8
leearvin <lhjdark@hotmail.com>
2016-10-09 13:45:23 +0800

    test

--- python/lovelypython/cdc.py
+++ python/lovelypython/cdc.py
@@ -6,4 +6,4 @@ import os
 #print os.walk('/media/lhj/sata')    #<generator object walk at 0x7fb765f85910>
 for root,dirs,files in os.walk("/media/lhj/sata"):
 	#print root,dirs,files
-	open('mycd.cdc','a').write("%s %s %s" % (root,dirs,files))   #format output
+	open('mycd.cdc','a').write("%s %s %s" % (root,dirs,files))   #format output111
