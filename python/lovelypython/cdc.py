bb50e89776a7882d1d3ccbd4a7dda925c4330458
leearvin <lhjdark@hotmail.com>
2016-10-09 13:32:32 +0800

    update cdc

--- python/lovelypython/cdc.py
+++ python/lovelypython/cdc.py
@@ -1,4 +1,9 @@
 #!/bin/bash
 # _*_ coding: utf-8 _*_
 import os
-print os.listdir("/media/lhj/sata")
+#print os.listdir("/media/lhj/sata")	#work
+#print os.listdir("/dev/sda1")		#not work
+#print os.walk('/media/lhj/sata')    #<generator object walk at 0x7fb765f85910>
+for root,dirs,files in os.walk("/media/lhj/sata"):
+	#print root,dirs,files
+	open('mycd.cdc','a').write("%s %s %s" % (root,dirs,files))   #format output
