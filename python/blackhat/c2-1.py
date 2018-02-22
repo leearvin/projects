5a292226e132b848b9c28a3ecab8bea2fb59b493
leearvin <lhjdark@hotmail.com>
2018-02-22 11:55:15 +0800

    111

--- python/blackhat/c2-1.py
+++ python/blackhat/c2-1.py
@@ -3,5 +3,3 @@ import socket
 target_host = 'www.baidu.com'
 target_prot = 80
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((target_host,target_prot))
-client.send("GET / HEEP/\r\nHost: google.com\r\n\
